7f934005955e84690d5d1c44c86b27e0cace7c88
szcrystal <vagrant@localhost.localdomain>
2015-06-08 03:35:40 +0000

    aaa

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe "AuthenticationPages" do
+    subject { page }
+
+      describe "signin page" do
+        before { visit signin_path }
+
+        it { should have_content('Sign in') }
+        it { should have_title('Sign in') }
+      end
+      
+      
+      describe "signin" do
+        before { visit signin_path }
+
+        describe "with invalid information" do
+          before { click_button "Sign in" }
+
+          it { should have_title('Sign in') }
+          it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+          
+          describe "after visiting another page" do
+              before { click_link "Home" }
+              it { should_not have_selector('div.alert.alert-error') }
+        	end
+          
+        end
+        
+        
+        describe "with valid information" do
+          let(:user) { FactoryGirl.create(:user) }
+          before {sign_in user}
+            
+
+          it { should have_title(user.name) }
+          it { should have_link('Profile', href: user_path(user)) }
+          it { should have_link('Settings',    href: edit_user_path(user)) }
+          it { should have_link('Sign out', href: signout_path) }
+          it { should_not have_link('Sign in', href: signin_path) }
+          
+          
+          
+          describe "followed by signout" do
+            before { click_link "Sign out" }
+            it { should have_link('Sign in') }
+          end
+          
+        end
+        
+      end #SingIn do
+      
+      
+#      describe "authorization" do
+#            
+#        describe "as non-admin user" do
+#          let(:user) { FactoryGirl.create(:user) }
+#          let(:non_admin) { FactoryGirl.create(:user) }
+#
+#          before { sign_in non_admin, no_capybara: true }
+#
+#          describe "submitting a DELETE request to the Users#destroy action" do
+#            before { delete user_path(user) }
+#            specify { expect(response).to redirect_to(root_path) }
+#          end
+#        end
+#      end
+
+      
+      
+end
