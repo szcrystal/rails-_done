5f5e246bcca46e235e68aea3254dc92bbca0c370
szcrystal <vagrant@localhost.localdomain>
2015-06-12 07:25:56 +0000

    Add follow

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,14 @@ describe User do
     
     it { should respond_to(:admin) }
 	it { should respond_to(:microposts) }
+    
+    it { should respond_to(:feed) }
+    
+  	it { should respond_to(:relationships) }
+    
+    it { should respond_to(:followed_users) }
+    it { should respond_to(:following?) }
+  	it { should respond_to(:follow!) }
 
   	it { should be_valid }
     
@@ -70,5 +78,24 @@ describe User do
         end
         
     end
+    
+    describe "following" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          @user.save
+          @user.follow!(other_user)
+        end
+
+        it { should be_following(other_user) }
+        its(:followed_users) { should include(other_user) }
+        
+        describe "and unfollowing" do
+          before { @user.unfollow!(other_user) }
+
+          it { should_not be_following(other_user) }
+          its(:followed_users) { should_not include(other_user) }
+        end
+        
+      end
 
 end
